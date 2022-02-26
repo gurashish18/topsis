e20d622112368c7fc8f3a36b99d6d928a4b5eeaa
Gurashish <ggill_be19@thapar.edu>
2022-02-26 09:58:22 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'TOPSIS method for Multiple-Criteria Decision Making'
+LONG_DESCRIPTION = 'TOPSIS method for Multiple-Criteria Decision Making long desc.'
+
+# Setting up
+setup(
+    name="gurashish's-topsis",
+    version=VERSION,
+    author="Gurashish Singh Gill",
+    author_email="<gillgurashish171@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'topsis', 'MCDM'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
